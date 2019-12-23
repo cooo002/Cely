d0898c1b5dc371d9a754c9f0cf293a2ed7908a45
cooo002 <cooo002@naver.com>
2019-12-24 00:26:24 +0900

    Initial Commit

---
+++ Cely/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cely
+//
+//  Created by 김재석 on 2019/12/24.
+//  Copyright © 2019 김재석. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
