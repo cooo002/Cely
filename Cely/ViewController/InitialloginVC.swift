76dac5eca65370dbb997d32a3891fa90ea9f0f5a
cooo002 <cooo002@naver.com>
2020-01-17 16:27:22 +0900

    Ver02 : f로그인 화면에 사용할 View배경 만들기

--- Cely/ViewController/InitialloginVC.swift
+++ Cely/ViewController/InitialloginVC.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import Pastel
 
 class InitialloginVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let pastelView = PastelView(frame: view.bounds)
+
+        // Custom Direction
+        pastelView.startPastelPoint = .bottomLeft
+        pastelView.endPastelPoint = .topRight
+
+        // Custom Duration
+        pastelView.animationDuration = 3.0
+
+        // Custom Color
+       pastelView.setColors([UIColor(red: 61/255, green: 61/255, blue: 61/255, alpha:                           1.0),
+                             UIColor(red: 50/255, green: 50/255, blue: 50/255, alpha: 1.0),
+                             UIColor(red: 40/255, green: 40/255, blue: 40/255, alpha: 1.0),
+                             UIColor(red: 20/255, green: 20/255, blue: 20/255, alpha: 1.0),])
+
+
+        pastelView.startAnimation()
+        view.insertSubview(pastelView, at: 0)
+        
     }
 
 
--- Podfile
+++ Podfile
@@ -12,6 +12,11 @@ target 'Cely' do
   # add pods for any other desired Firebase products
   # https://firebase.google.com/docs/ios/setup#available-pods
 
+  # add To Design OpenSource 
+   pod "Pastel"
+
+
+
 
   target 'CelyTests' do
     inherit! :search_paths
