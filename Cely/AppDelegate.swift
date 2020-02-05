4956e4483480945e7f997eafd64bb9b7b8891561
cooo002 <cooo002@naver.com>
2020-02-06 04:19:04 +0900

    ver07: 충돌해결2

--- Cely/AppDelegate.swift
+++ Cely/AppDelegate.swift
@@ -10,13 +10,13 @@ import UIKit
 import CoreData
 import Firebase
 import GoogleSignIn
-<<<<<<< HEAD
-
-
-
-=======
-
->>>>>>> a949296f8848fcbf7f5d0c4d9b86a92677e55e7e
+//<<<<<<< HEAD
+//
+//
+//
+//=======
+//
+//>>>>>>> a949296f8848fcbf7f5d0c4d9b86a92677e55e7e
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
     
@@ -30,13 +30,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
         GIDSignIn.sharedInstance()?.clientID = FirebaseApp.app()?.options.clientID
         GIDSignIn.sharedInstance().delegate = self
         
-<<<<<<< HEAD
-
-        
-        
-=======
->>>>>>> a949296f8848fcbf7f5d0c4d9b86a92677e55e7e
-        
+//<<<<<<< HEAD
+//
+//        
+//        
+//=======
+//>>>>>>> a949296f8848fcbf7f5d0c4d9b86a92677e55e7e
+//        
 
         return true
     }
@@ -148,13 +148,13 @@ extension AppDelegate{
     }
     
 }
-<<<<<<< HEAD
-
-
-//Check:faceBook로그인 관련 메소드
-extension AppDelegate{
-
-}
-=======
->>>>>>> a949296f8848fcbf7f5d0c4d9b86a92677e55e7e
-
+//<<<<<<< HEAD
+//
+//
+////Check:faceBook로그인 관련 메소드
+//extension AppDelegate{
+//
+//}
+//=======
+//>>>>>>> a949296f8848fcbf7f5d0c4d9b86a92677e55e7e
+//
