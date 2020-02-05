10024cebcefc9335427a00fb4b9488c62341fb0e
cooo002 <cooo002@naver.com>
2020-02-05 14:04:33 +0900

    Ver04:SelectLoginVC완성 & 필요한 pod파일추가

---
+++ Cely/ViewController/RootInitVC.swift
@@ -0,0 +1,30 @@
+//
+//  RootInitVC.swift
+//  Cely
+//
+//  Created by 김재석 on 2020/01/20.
+//  Copyright © 2020 김재석. All rights reserved.
+//
+
+import UIKit
+
+class RootInitVC: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
